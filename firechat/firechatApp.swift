76c1badc50a8beea4f7313aa861e1756fde18749
404paigenotfound <92485057+paigethompson150@users.noreply.github.com>
2024-03-28 19:07:34 -0400

    Initial Commit

---
+++ firechat/firechatApp.swift
@@ -0,0 +1,17 @@
+//
+//  firechatApp.swift
+//  firechat
+//
+//  Created by Paige Thompson on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct firechatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
